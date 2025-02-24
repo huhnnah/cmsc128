ce13c4ac858d1757882d3a320f68ee6996867e10
Hannah Ramos <hmramos1@up.edu.ph>
2025-02-25 01:20:46 +0800

    Init commit

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+'use client'
+import Image from "next/image";
+import Link from 'next/link';
+import LoginForm from "@/components/LoginForm";
+import { Button } from "@/components/ui/button";
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="w-full max-w-sm">
+      <LoginForm></LoginForm>
+      <Button onClick={() => router.push('/dashboard')}>Punta ka na beh</Button>
+    </div>
+  </div>
+  );
+}
